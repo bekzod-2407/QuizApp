3f32a655bda44e443dd896735811192ddac5f754
Bekzod <bekzod.k2407@gmail.com>
2022-07-20 09:29:03 +0500

    added views on vc

--- QuizApp/MainViewController.swift
+++ QuizApp/MainViewController.swift
@@ -9,12 +9,30 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    private lazy var mainContentView:  MainView =  {
+        var view = MainView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
+        setupSubViews()
+        view.backgroundColor = Colors.mainColor.color
     }
 
 
+    private func setupSubViews() {
+        view.addSubview(mainContentView)
+        
+        NSLayoutConstraint.activate([
+            mainContentView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainContentView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            mainContentView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            mainContentView.bottomAnchor.constraint(equalTo:view.safeAreaLayoutGuide.bottomAnchor ),
+            
+
+        ])
+    }
 }
 
--- QuizApp/Views/MainView.swift
+++ QuizApp/Views/MainView.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class MainView: UIView {
+    private lazy var mainStack: UIStackView = {
+        var view = UIStackView(arrangedSubviews: [scoreLabel,questionLabel,buttonStack])
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.spacing = 20
+        view.distribution = .fill
+        return view
+    }()
+
  
     private lazy var scoreLabel: UILabel = {
         var view = UILabel()
@@ -15,6 +24,8 @@ class MainView: UIView {
         view.text = "Score 0"
         view.font = .monospacedSystemFont(ofSize: 18, weight: .medium)
         view.textColor = .systemGray6
+        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        view.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
         return view
     }()
     
@@ -40,13 +51,24 @@ class MainView: UIView {
     private lazy var trueButton: UIButton = {
         var view = UIButton()
         view.translatesAutoresizingMaskIntoConstraints = false
-    
+        view.setTitle("TRUE", for: .normal)
+        view.tintColor = .black
+        view.titleLabel?.font = .monospacedSystemFont(ofSize: 18, weight: .semibold)
+        view.layer.borderWidth = 3
+        view.layer.borderColor = CGColor(red: 107, green: 219, blue: 153, alpha: 1)
+        view.layer.cornerRadius = 15
+        view.clipsToBounds = true
         return view
     }()
     private lazy var falseButton: UIButton = {
         var view = UIButton()
         view.translatesAutoresizingMaskIntoConstraints = false
-     
+        view.setTitle("FALSE", for: .normal)
+        view.titleLabel?.font = .monospacedSystemFont(ofSize: 18, weight: .semibold)
+        view.layer.borderWidth = 3
+        view.layer.borderColor = CGColor(red: 107, green: 215, blue: 153, alpha: 1)
+        view.layer.cornerRadius = 15
+        view.clipsToBounds = true
         return view
     }()
     
@@ -55,12 +77,26 @@ class MainView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupSubViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+       
     }
     
-    
+    private func setupSubViews() {
+        self.addSubview(mainStack)
+        
+        NSLayoutConstraint.activate([
+            mainStack.topAnchor.constraint(equalTo: self.topAnchor,constant: 10),
+            mainStack.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 20),
+            mainStack.rightAnchor.constraint(equalTo: self.rightAnchor,constant: -20),
+            mainStack.bottomAnchor.constraint(equalTo: self.bottomAnchor,constant: -10),
+            
+            trueButton.heightAnchor.constraint(equalToConstant: 60),
+            falseButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
 
 }
